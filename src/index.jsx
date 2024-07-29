243aeaf68d0659880a865385bcaf4c20c45842c4
bruno.grosdidier@2dcom.fr <bruno.grosdidier@2dcom.fr>
2024-07-29 15:01:24 +0200

    Correction rename 'home' folder to 'Home' #develop

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/home';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './pages/Home'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <Home/>
-    </React.StrictMode>
-);
-
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
